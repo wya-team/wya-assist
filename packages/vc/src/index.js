9a67ee57ca72a4e79c9257f76ca8ad07e005f1f3
Mr.Z <zrd0921@qq.com>
2020-09-22 19:48:20 +0800

    feat: auto install with vue

--- packages/vc/src/index.js
+++ packages/vc/src/index.js
@@ -1,3 +1,12 @@
 export { default as SectionTitle } from './section-title';
 export { default as Footer } from './footer';
 export { default as Table } from './table';
+
+export default {
+	install(Vue, opts) {
+		Vue.component(SectionTitle.name, SectionTitle);
+		Vue.component(Footer.name, Footer);
+		Vue.component(Table.Filter.name, Table.Filter);
+		Vue.component(Table.Sorter.name, Table.Sorter);
+	}
+};
