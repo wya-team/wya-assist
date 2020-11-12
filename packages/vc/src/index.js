30417756c8e4ed40de7df7df903f307316c9c552
Mr.Z <zrd0921@qq.com>
2020-11-12 21:29:37 +0800

    fix(vc): link

--- packages/vc/src/index.js
+++ packages/vc/src/index.js
@@ -2,18 +2,21 @@ import SectionTitle from './section-title';
 import Footer from './footer';
 import Table from './table';
 import Gallery from './gallery';
+import Link from './link';
 
 export {
 	SectionTitle,
 	Footer,
 	Table,
-	Gallery
+	Gallery,
+	Link
 };
 
 export default {
 	install(Vue, opts) {
 		Vue.component(SectionTitle.name, SectionTitle);
 		Vue.component(Footer.name, Footer);
+		Vue.component(Link.name, Link);
 		
 		Vue.component(Table.Filter.name, Table.Filter);
 		Vue.component(Table.Sorter.name, Table.Sorter);
