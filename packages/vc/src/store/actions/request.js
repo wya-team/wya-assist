685b2745a6fee96d5d08518a80d3aa9601b8f4c5
Mr.Z <zrd0921@qq.com>
2021-07-23 09:57:14 +0800

    feat(VC): [Store.Actions.request] redirect support more types

--- packages/vc/src/store/actions/request.js
+++ packages/vc/src/store/actions/request.js
@@ -1,3 +1,19 @@
+const getCustomMutation = (data, type) => {
+	if (!data) return;
+
+	if (typeof data === 'string') {
+		return data;
+	}
+
+	if (typeof data === 'object') {
+		return data[type];
+	}
+
+	if (typeof data === 'function') {
+		return data(type);
+	}
+};
+
 const requestAction = (defaultOptions = {}) => {
 	const { apis, request } = defaultOptions;
 	return (store, opts = {}) => {
@@ -18,7 +34,7 @@ const requestAction = (defaultOptions = {}) => {
 		}
 		
 		// pending 为 false，则必须要写_PENDING的mutation
-		pending && store.commit(redirect || `${mutation}_PENDING`, { param });
+		pending && store.commit(getCustomMutation(redirect, 'pending') || `${mutation}_PENDING`, { param });
 		return request({
 			url: apis[mutation],
 			param,
@@ -26,14 +42,14 @@ const requestAction = (defaultOptions = {}) => {
 			...rest
 		}).then((res) => {
 			const { data } = res;
-			store.commit(redirect || `${mutation}_${refresh ? 'REFRESH' : 'SUCCESS'}`, {
+			store.commit(getCustomMutation(redirect, refresh ? 'refresh' : 'success') || `${mutation}_${refresh ? 'REFRESH' : 'SUCCESS'}`, {
 				data,
 				param,
 				// ...rest
 			});
 			return res;
 		}).catch((error) => {
-			fail && store.commit(redirect || `${mutation}_FAIL`, { param });
+			fail && store.commit(getCustomMutation(redirect, 'fail') || `${mutation}_FAIL`, { param });
 			return Promise.reject(error);
 		});
 	};
